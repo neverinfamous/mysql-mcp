fc3bdaa315226a0b32d0ce9221c363b0bb95458d
Chris <admin@adamic.tech>
2026-06-17 22:22:08 -0400

    chore: remove temporary scratch scripts

--- test-zod.ts
+++
@@ -1,3 +0,0 @@
-import { FulltextCreateSchema } from "./src/adapters/mysql/schemas/text/fulltext.js";
-const result = FulltextCreateSchema.parse({table: "test_users", columns: ["bio"], name: "ft_bio_idx"});
-console.log(JSON.stringify(result, null, 2));
