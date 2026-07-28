e866d1a1f6340c1b675d8d5f5080de1e763f7142
Chris <admin@adamic.tech>
2026-07-27 21:28:51 -0400

    Delete scratch file left by Gemini 3.1 Pro.

--- test-schema.ts
+++
@@ -1,6 +0,0 @@
-import { ProxySQLHostgroupInputSchema } from './src/adapters/mysql/schemas/proxysql.js';
-try {
-  console.log(ProxySQLHostgroupInputSchema.parse({ hostgroup_id: '1' }));
-} catch (e) {
-  console.log(e.toString());
-}
